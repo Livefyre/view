041517adb42372eee379dbafd04500be335d1897
Ryan Cheung <ryanc@livefyre.com>
2014-05-20 13:13:52 -0700

    Composite view

---
+++ src/composite-view.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var View = require('view');
+var inherits = require('inherits');
+
+var CompositeView = function () {
+    var args = Array.prototype.slice.call(arguments, 0)
+    var hasOpts = !(args[args.length-1] instanceof View);
+    var opts = hasOpts ? args.pop() : {};
+
+    View.call(this, opts);
+
+    this._childViews = [];
+
+    var subViews = hasOpts ? args.slice(0, args.length) : args;
+    for (var i=0; i < subViews.length; i++) {
+        this.addView(subViews[i]);
+    }
+};
+inherits(CompositeView, View);
+
+CompositeView.prototype.addView = function (view) {
+    this._childViews.push(view);
+    this.$el.append(view.$el);
+    view.render();
+    return this;
+};
+
+CompositeView.prototype.removeView = function (view) {
+    view.destroy();
+    this._childViews.splice(this._childViews.indexOf(view), 1);
+    return this;
+};
+
+CompositeView.prototype.render = function () {
+    for (var i=0; i < this._childViews.length; i++) {
+        this._childViews[i].render();
+    }
+    return this;
+};
+
+module.exports = CompositeView;
